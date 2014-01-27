25bdeaf5a95899e20d65ca76bbb09491d6c0c4d9
rutkoski <rutkoski@gmail.com>
2014-01-27 14:53:15 -0200

    Added IE compatibility.

--- lib/JSON.php
+++ lib/JSON.php
@@ -3,9 +3,13 @@
 class JSON
 {
 
-  public static function output($response)
+  public static function output($response, $compat = true)
   {
-    header('Content-type: application/json; charset="utf-8"');
+    if ($compat) {
+      header('Content-type: text/html; charset="utf-8"');
+    } else {
+      header('Content-type: application/json; charset="utf-8"');
+    }
     echo json_encode($response);
   }
 
